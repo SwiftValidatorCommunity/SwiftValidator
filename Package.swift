cc9c6c9920220a6872e63e300613e62b68f37e5e
Lester Batres <lester96@gmail.com>
2020-09-19 22:42:53 -0600

    SPM Support
    -Added iOS version.
    -Added dependency.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftValidator",
+    platforms: [.iOS(.v8)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftValidator",
-            dependencies: []),
+            dependencies: ["UIKit"]),
         .testTarget(
             name: "SwiftValidatorTests",
             dependencies: ["SwiftValidator"]),
