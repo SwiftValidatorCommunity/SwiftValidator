d38d9b2d5b9d5cccf90510d8363b1b5977208d7e
Lester Batres <lester96@gmail.com>
2020-09-19 22:46:19 -0600

    SPM Support
    -UIKit

--- Core/Validatable.swift
+++ Core/Validatable.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 public typealias ValidatableField = AnyObject & Validatable
 
--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftValidator",
-            dependencies: ["UIKit"]),
+            dependencies: []),
         .testTarget(
             name: "SwiftValidatorTests",
             dependencies: ["SwiftValidator"]),
