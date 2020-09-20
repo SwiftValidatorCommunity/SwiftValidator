17f62d03082fd292a9956a01dd5787f5efd51f61
Lester Batres <lester96@gmail.com>
2020-09-19 21:18:53 -0600

    SPM support
    -Init Swift Package Manager

---
+++ Sources/SwiftValidator/SwiftValidator.swift
@@ -0,0 +1,3 @@
+struct SwiftValidator {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SwiftValidatorTests
+
+var tests = [XCTestCaseEntry]()
+tests += SwiftValidatorTests.allTests()
+XCTMain(tests)
